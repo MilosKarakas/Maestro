c12fd92cf30578656bafc6c96f78f6e7e83e1c26
Miloš Karakaš <milos.karakas@gmail.com>
2025-12-16 14:58:34 +0100

    fix: add missing SwipeDirection import and fix parameter name

--- maestro-orchestra/src/main/java/maestro/orchestra/Orchestra.kt
+++ maestro-orchestra/src/main/java/maestro/orchestra/Orchestra.kt
@@ -44,6 +44,7 @@ import maestro.orchestra.geo.Traveller
 import maestro.orchestra.util.calculateElementRelativePoint
 import maestro.orchestra.util.Env.evaluateScripts
 import maestro.orchestra.yaml.YamlCommandReader
+import maestro.SwipeDirection
 import maestro.toSwipeDirection
 import maestro.utils.Insight
 import maestro.utils.Insights
@@ -625,7 +626,7 @@ class Orchestra(
             } else {
                 // Element selector format - find the element and scroll within its bounds
                 val selector = ElementSelector(textRegex = scrollPoint)
-                val element = findElement(selector, optional = false, timeout = 2000).element
+                val element = findElement(selector, optional = false, timeoutMs = 2000).element
                 maestro.swipe(
                     SwipeDirection.UP,
                     element,
